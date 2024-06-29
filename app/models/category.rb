986d30ea818ada6a92fcf3d6e160671ef21824a9
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-06-29 08:14:31 -0500

    feat: generate core models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
+# Represents a category of products.
+# Attributes:
+#   - name (string, required): The name of the category.
+# Relationships:
+#   - has_many :products: The products belonging to this category.
+# Validations:
+#   - name:
+#     - must be present
+#     - must be unique
 class Category < ApplicationRecord
+  has_many :products, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
 end
