8878edf9feb0ff4388fc1869a920696d4d1585d3
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-08-10 03:54:45 -0500

    feat: transition to GraphQL API
    - Removed REST API endpoints for categories and products
    - Added GraphQL configuration with support for querying products and categories
    - Mounted GraphiQL engine for development at `/graphiql`
    - Updated routes to handle GraphQL requests
    - Removed associated REST API tests and helpers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,22 +1,9 @@
 # frozen_string_literal: true
 
-# Represents a product that can be purchased.
-
-# Attributes:
-#   - name (string, required): The name of the product.
-#   - price (decimal, optional): The price of the product.
-#   - features (JSON, optional): Additional features of the product, stored as a JSON object.
-# Relationships:
-#   - belongs_to :category: The category to which this product belongs.
-# Validations:
-#   - name:
-#     - must be present
-#     - must be between 1 and 255 characters long
-#   - price:
-#     - must be a number greater than or equal to 0
-#     - can be nil (if the product has no price)
 class Product < ApplicationRecord
   belongs_to :category
+  # has_one_attached :image
+
   validates :name, presence: true, length: { minimum: 1, maximum: 255 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
   serialize :features, JSON
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
-require "active_support/core_ext/integer/time"
+# frozen_string_literal: true
+
+require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -41,57 +43,5 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
-  # Assume all access to the app is happening through a SSL-terminating reverse proxy.
-  # Can be used together with config.force_ssl for Strict-Transport-Security and secure cookies.
-  # config.assume_ssl = true
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
-
-  # Log to STDOUT by default
-  config.logger = ActiveSupport::Logger.new(STDOUT)
-    .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
-    .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
-
-  # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
-
-  # "info" includes generic and useful information about system operation, but avoids logging too much
-  # information to avoid inadvertent exposure of personally identifiable information (PII). If you
-  # want to log everything, set the level to "debug".
-  config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "apace_systems_backend_production"
-
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
-
-  # Don't log any deprecations.
-  config.active_support.report_deprecations = false
-
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
-
-  # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
-  # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  # config.action_cable.url = "
 end
