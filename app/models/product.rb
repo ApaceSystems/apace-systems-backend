d2af72f83277357ae159b552db65e99822e66cfc
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-09-12 13:49:10 -0500

    refactor: adjust product model for testing configuration
    - Updated product model to improve test compatibility.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,5 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 1, maximum: 255 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  serialize :features, JSON
+  serialize :features, coder: JSON
 end
