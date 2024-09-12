83ba8fecf64302de41002d0c22ea5e519995239a
rohanbhadreshpatel <rohanbpatel98@gmail.com>
2024-09-12 17:32:19 -0500

    fix: resolve CI/CD pipeline and secrets loading issue
    - Finalized CI/CD pipeline configuration
    - Resolved issues with loading CI/CD secrets

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,27 +12,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_29_104453) do
+ActiveRecord::Schema[7.1].define(version: 20_240_629_104_453) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.decimal "price"
-    t.bigint "category_id", null: false
-    t.text "features"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
+  create_table 'products', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.decimal 'price'
+    t.bigint 'category_id', null: false
+    t.text 'features'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['category_id'], name: 'index_products_on_category_id'
   end
 
-  add_foreign_key "products", "categories"
+  add_foreign_key 'products', 'categories'
 end
